57a1189f6b157dfd2f8dc2827eda0f68e6a99534
Rene Zurita <rene@imrsv.se>
2017-05-14 18:23:07 +0100

    Drag Functionality
    Added drag controls

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,48 +3,64 @@ using UnityEngine.UI;
 using System.Collections;
 using System;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Character : MonoBehaviour
 {
-
-	private UnityEngine.AI.NavMeshAgent agent;
-	public string CharacterName;
-	public Sprite Portrait;
-	public float speed = 5;
-	public Skill PrimarySkill;
-	public Skill SecondarySkill;
-	public float MaxHealth = 100;
-	public float CurrentHealth = 100;
+    private LineRenderer lRenderer;
+    private UnityEngine.AI.NavMeshAgent agent;
+    public string CharacterName;
+    public Sprite Portrait;
+    public float speed = 5;
+    public Skill PrimarySkill;
+    public Skill SecondarySkill;
+    public float MaxHealth = 100;
+    public float CurrentHealth = 100;
     public float AttackRange;
     public float HitBoxRadius = 0.5f;
+    private bool isDragging;
 
-    void Start ()
-	{
-		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
-		agent.speed = speed;
-		GameController.Handle.AddCharacter (this);
-	}
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.speed = speed;
+        GameController.Handle.AddCharacter(this);
+        lRenderer = GetComponent<LineRenderer>() ?? gameObject.AddComponent<LineRenderer>();
+    }
 
-	public void GoTo (Vector3 destination)
-	{
-		agent.SetDestination (destination);
-		print ("going to: " + destination);
-	}
+    private void Update()
+    {
+        if (isDragging)
+        {
+            lRenderer.SetPosition(0, transform.position);
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 10000))
+            {
+                lRenderer.SetPosition(1, hit.point);
+            }
+        }
+        else
+        {
+            lRenderer.SetPosition(0, transform.position);
+            lRenderer.SetPosition(1, transform.position);
+        }
+    }
 
-	public void UsePrimarySkill ()
-	{
-		print (CharacterName + "used: " + PrimarySkill.Name + "(PS)");
-	}
+    #region Actions
+    public void GoTo(Vector3 destination)
+    {
+        agent.SetDestination(destination);
+        print("going to: " + destination);
+    }
 
-	public void UseSecondarySkill ()
-	{
-		print (CharacterName + "used: " + SecondarySkill.Name + "(SS)");
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
+    public void UsePrimarySkill()
+    {
+        print(CharacterName + "used: " + PrimarySkill.Name + "(PS)");
+    }
+
+    public void UseSecondarySkill()
+    {
+        print(CharacterName + "used: " + SecondarySkill.Name + "(SS)");
+    }
 
     public void Engage(Enemy enemy)
     {
@@ -53,4 +69,51 @@ public class Character : MonoBehaviour
         agent.autoBraking = true;
         print("Engaging: " + enemy.Name + " Stop: " + CombatHelper.GetCombatDistance(this, enemy));
     }
+
+    private void Assist(Character character)
+    {
+        //TODO: Implement for healers.
+        print("Assisting: " + character.CharacterName);
+    }
+    #endregion
+
+    #region Drag controls
+    void OnMouseDrag()
+    {
+        isDragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        if (isDragging)
+        {
+            OnDragRelease();
+            isDragging = false;
+        }
+    }
+
+    private void OnDragRelease()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 10000))
+        {
+            if (ClickHelper.IsUIClick())
+                return;
+            if (ClickHelper.IsCharacter(hit))
+            {
+                Assist(hit.transform.GetComponent<Character>());
+            }
+            else if (ClickHelper.IsEnemy(hit))
+            {
+                Engage(hit.transform.GetComponent<Enemy>());
+            }
+            else if (ClickHelper.IsTerrain(hit))
+            {
+                GoTo(hit.point);
+            }
+        }
+    }
+    #endregion
+
+
 }
